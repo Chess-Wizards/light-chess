6a84ad2e3e1dd2d8bb5811add2adfe8c3220a4fc
Mikhail Amirov <amirovm@google.com>
2020-12-22 00:29:33 +0100

    Migrate to Colour class. + Normalize imports.

--- entities/board.py
+++ entities/board.py
@@ -4,8 +4,9 @@ from __future__ import annotations
 
 from collections import defaultdict
 
-from .pieces import Piece, PieceType, PieceColour
-from .position import Position
+from entities.colour import Colour
+from entities.pieces import Piece, PieceType
+from entities.position import Position
 from typing import List, Optional
 
 class Board(object):
@@ -14,7 +15,7 @@ class Board(object):
 	"""
 
 	def __init__(self) -> None:
-		# Stores mapping from unique pairs of (piece_type, piece_pos)
+		# Stores mapping from unique pairs of (piece_type, colour)
 		# to a set of positions.
 		# Used for getting all positions for a specific piece type.
 		self._piece_to_pos = defaultdict(set)
@@ -60,42 +61,42 @@ class Board(object):
 		board = Board()
 
 		# Set white pieces.
-		board.set_piece(Position(0, 0), Piece(PieceType.Rook, PieceColour.White))
-		board.set_piece(Position(1, 0), Piece(PieceType.Knight, PieceColour.White))
-		board.set_piece(Position(2, 0), Piece(PieceType.Bishop, PieceColour.White))
-		board.set_piece(Position(3, 0), Piece(PieceType.Queen, PieceColour.White))
-		board.set_piece(Position(4, 0), Piece(PieceType.King, PieceColour.White))
-		board.set_piece(Position(5, 0), Piece(PieceType.Bishop, PieceColour.White))
-		board.set_piece(Position(6, 0), Piece(PieceType.Knight, PieceColour.White))
-		board.set_piece(Position(7, 0), Piece(PieceType.Rook, PieceColour.White))
+		board.set_piece(Position(0, 0), Piece(PieceType.Rook, Colour.WHITE))
+		board.set_piece(Position(1, 0), Piece(PieceType.Knight, Colour.WHITE))
+		board.set_piece(Position(2, 0), Piece(PieceType.Bishop, Colour.WHITE))
+		board.set_piece(Position(3, 0), Piece(PieceType.Queen, Colour.WHITE))
+		board.set_piece(Position(4, 0), Piece(PieceType.King, Colour.WHITE))
+		board.set_piece(Position(5, 0), Piece(PieceType.Bishop, Colour.WHITE))
+		board.set_piece(Position(6, 0), Piece(PieceType.Knight, Colour.WHITE))
+		board.set_piece(Position(7, 0), Piece(PieceType.Rook, Colour.WHITE))
 
-		board.set_piece(Position(0, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(1, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(2, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(3, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(4, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(5, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(6, 1), Piece(PieceType.Pawn, PieceColour.White))
-		board.set_piece(Position(7, 1), Piece(PieceType.Pawn, PieceColour.White))
+		board.set_piece(Position(0, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(1, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(2, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(3, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(4, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(5, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(6, 1), Piece(PieceType.Pawn, Colour.WHITE))
+		board.set_piece(Position(7, 1), Piece(PieceType.Pawn, Colour.WHITE))
 
 
 		# Set black pieces.
-		board.set_piece(Position(0, 7), Piece(PieceType.Rook, PieceColour.Black))
-		board.set_piece(Position(1, 7), Piece(PieceType.Knight, PieceColour.Black))
-		board.set_piece(Position(2, 7), Piece(PieceType.Bishop, PieceColour.Black))
-		board.set_piece(Position(3, 7), Piece(PieceType.Queen, PieceColour.Black))
-		board.set_piece(Position(4, 7), Piece(PieceType.King, PieceColour.Black))
-		board.set_piece(Position(5, 7), Piece(PieceType.Bishop, PieceColour.Black))
-		board.set_piece(Position(6, 7), Piece(PieceType.Knight, PieceColour.Black))
-		board.set_piece(Position(7, 7), Piece(PieceType.Rook, PieceColour.Black))
+		board.set_piece(Position(0, 7), Piece(PieceType.Rook, Colour.BLACK))
+		board.set_piece(Position(1, 7), Piece(PieceType.Knight, Colour.BLACK))
+		board.set_piece(Position(2, 7), Piece(PieceType.Bishop, Colour.BLACK))
+		board.set_piece(Position(3, 7), Piece(PieceType.Queen, Colour.BLACK))
+		board.set_piece(Position(4, 7), Piece(PieceType.King, Colour.BLACK))
+		board.set_piece(Position(5, 7), Piece(PieceType.Bishop, Colour.BLACK))
+		board.set_piece(Position(6, 7), Piece(PieceType.Knight, Colour.BLACK))
+		board.set_piece(Position(7, 7), Piece(PieceType.Rook, Colour.BLACK))
 
-		board.set_piece(Position(0, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(1, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(2, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(3, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(4, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(5, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(6, 6), Piece(PieceType.Pawn, PieceColour.Black))
-		board.set_piece(Position(7, 6), Piece(PieceType.Pawn, PieceColour.Black))
+		board.set_piece(Position(0, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(1, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(2, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(3, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(4, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(5, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(6, 6), Piece(PieceType.Pawn, Colour.BLACK))
+		board.set_piece(Position(7, 6), Piece(PieceType.Pawn, Colour.BLACK))
 
 		return board
