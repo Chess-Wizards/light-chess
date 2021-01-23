96833d109617bf74127f91f81f3fb60051ddbebd
Andrey Kurzyukov <andrey.kurzyukov@fau.de>
2021-01-24 00:30:00 +0100

    Empty

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 
 from engine.game import Game
 from entities.board import Board
-from entities.position import Position
 
 
 def main():
-	start_board = Board.create_start_board()
-	game = Game.create_start_game()
-            
+    _ = Board.create_start_board()
+    _ = Game.create_start_game()
 
-if __name__ == '__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
