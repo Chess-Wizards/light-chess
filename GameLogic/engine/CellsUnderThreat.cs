bdbb0faffd7d27edef8892fe3a3d965fdb21c11b
SherlockKA <kurzyukovandrew@mail.ru>
2022-07-11 16:56:43 +0200

    Undo changes

--- GameLogic/engine/CellsUnderThreat.cs
+++ GameLogic/engine/CellsUnderThreat.cs
@@ -4,29 +4,27 @@ using System.Collections.Generic;
 
 namespace GameLogic
 {
+    // The static class aims to find a list of cells 'under threat'. 'under threat' means all cells
+    // at which the enemy king cannot stand because of the check. In addition, the piece must be able to 
+    // make a move at this cell. For example, a pawn can capture iff the enemy piece stands at a diagonal.
+    //
+    // This class does not consider checks, en passant moves, and castles.
     static public class CellsUnderThreat
     {
-        // The static class aims to find a list of cells 'under threat'. 'under threat' means all cells
-        // at which the enemy king cannot stand because of the check. In addition, the piece must be able to 
-        // make a move at this cell. For example, a pawn can capture iff the enemy piece stands at a diagonal.
-        //
-        // This class does not consider checks, en passant moves, and castles.
 
+        // Finds cells under threat produced by piece at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // board: The board represents the current arrangement of all pieces.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by piece at cell |cell|.
         public static List<Cell> GetCellsUnderThreat(Cell cell,
                                                      StandardBoard board)
         {
-            // Finds cells under threat produced by piece at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // board: The board represents the current arrangement of all pieces.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by piece at cell |cell|.
-
-
             var piece = board[cell];
 
             // Return empty list if cell is empty.
@@ -58,34 +56,33 @@ namespace GameLogic
             return mappingPieceTypeToMethod[(PieceType)piece?.Type](cell,
                                             pieceCells,
                                             enemyPieceCells,
-                                            board.OnBoard,
+                                            board.IsOnBoard,
                                             (Color)piece?.Color);
         }
 
+        // Finds cells under threat produced by piece at cell |cell|. 
+        // This function is a helper one for other methods.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // shifts: A list containing possible cell shifts.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // oneShift: Check only |shifts| if true, otherwise perform shifts until the cells are on board.
+        // For example, |oneShift| is true for bishop, rook, and queen, while false for pawn, knight, and king.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by piece at cell |cell|. 
         public static List<Cell> FindCells(Cell cell,
                                            List<Cell> shifts,
                                            List<Cell> pieceCells,
                                            List<Cell> enemyPieceCells,
-                                           Func<Cell, bool> OnBoard,
+                                           Func<Cell, bool> IsOnBoard,
                                            bool oneShift = false)
         {
-            // Finds cells under threat produced by piece at cell |cell|. 
-            // This function is a helper one for other methods.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // shifts: A list containing possible cell shifts.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // oneShift: Check only |shifts| if true, otherwise perform shifts until the cells are on board.
-            // For example, |oneShift| is true for bishop, rook, and queen, while false for pawn, knight, and king.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by piece at cell |cell|. 
-
             // List to save cells 'under threat'
             var cellsUnderThreat = new List<Cell>();
 
@@ -97,7 +94,7 @@ namespace GameLogic
                 while (true)
                 {
                     currentCell = currentCell + shift;
-                    if (!OnBoard(currentCell)
+                    if (!IsOnBoard(currentCell)
                         || pieceCells.Contains(currentCell)) break;
 
                     cellsUnderThreat.Add(currentCell);
@@ -109,26 +106,25 @@ namespace GameLogic
             return cellsUnderThreat;
         }
 
+        // Finds cells under threat produced by rook at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The rook color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by rook at cell |cell|.
         public static List<Cell> GetCellsUnderThreatRook(Cell cell,
                                                          List<Cell> pieceCells,
                                                          List<Cell> enemyPieceCells,
-                                                         Func<Cell, bool> OnBoard,
+                                                         Func<Cell, bool> IsOnBoard,
                                                          Color activeColor)
         {
-            // Finds cells under threat produced by rook at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The rook color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by rook at cell |cell|. 
-
             // up and down - y-axis or height
             // right and left - x-axis or width
             var upShift = new Cell(0, 1);
@@ -147,30 +143,29 @@ namespace GameLogic
                              shifts,
                              pieceCells,
                              enemyPieceCells,
-                             OnBoard,
+                             IsOnBoard,
                              oneShift: false);
         }
 
+        // Finds cells under threat produced by knight at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The knight color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by knight at cell |cell|.
         public static List<Cell> GetCellsUnderThreatKnight(Cell cell,
                                                            List<Cell> pieceCells,
                                                            List<Cell> enemyPieceCells,
-                                                           Func<Cell, bool> OnBoard,
+                                                           Func<Cell, bool> IsOnBoard,
                                                            Color activeColor)
         {
-            // Finds cells under threat produced by knight at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The knight color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by knight at cell |cell|.
-
             var xs = new[] { -2, -1, 1, 2 };
             var ys = new[] { -2, -1, 1, 2 };
             var shifts =
@@ -185,32 +180,31 @@ namespace GameLogic
                              shifts,
                              pieceCells,
                              enemyPieceCells,
-                             OnBoard,
+                             IsOnBoard,
                              oneShift: true);
 
         }
 
+        // Finds cells under threat produced by bishop at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The bishop color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by bishop at cell |cell|.
         public static List<Cell> GetCellsUnderThreatBishop(Cell cell,
                                                            List<Cell> pieceCells,
                                                            List<Cell> enemyPieceCells,
-                                                           Func<Cell, bool> OnBoard,
+                                                           Func<Cell, bool> IsOnBoard,
                                                            Color activeColor)
 
         {
-            // Finds cells under threat produced by bishop at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The bishop color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by bishop at cell |cell|.
-
             // up and down - y-axis or height.
             // right and left - x-axis or width.
             var upRightShift = new Cell(1, 1);
@@ -230,66 +224,64 @@ namespace GameLogic
                              shifts,
                              pieceCells,
                              enemyPieceCells,
-                             OnBoard,
+                             IsOnBoard,
                              oneShift: false);
         }
 
+        // Finds cells under threat produced by queen at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The queen color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by queen at cell |cell|.
         public static List<Cell> GetCellsUnderThreatQueen(Cell cell,
                                                           List<Cell> pieceCells,
                                                           List<Cell> enemyPieceCells,
-                                                          Func<Cell, bool> OnBoard,
+                                                          Func<Cell, bool> IsOnBoard,
                                                           Color activeColor)
 
         {
-            // Finds cells under threat produced by queen at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The queen color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by queen at cell |cell|.
-
             var cellsUnderThreatRook = GetCellsUnderThreatRook(cell,
                                                                pieceCells,
                                                                enemyPieceCells,
-                                                               OnBoard,
+                                                               IsOnBoard,
                                                                activeColor);
 
             var cellsUnderThreatBishop = GetCellsUnderThreatBishop(cell,
                                                                    pieceCells,
                                                                    enemyPieceCells,
-                                                                   OnBoard,
+                                                                   IsOnBoard,
                                                                    activeColor);
             // Concatenate/combine rook and bishop cells. 
             return cellsUnderThreatRook.Concat(cellsUnderThreatBishop).ToList(); ;
         }
 
+        // Finds cells under threat produced by king at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The king color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by king at cell |cell|.
         public static List<Cell> GetCellsUnderThreatKing(Cell cell,
                                                          List<Cell> pieceCells,
                                                          List<Cell> enemyPieceCells,
-                                                         Func<Cell, bool> OnBoard,
+                                                         Func<Cell, bool> IsOnBoard,
                                                          Color activeColor)
         {
-            // Finds cells under threat produced by king at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The king color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by king at cell |cell|.
-
             var xs = new[] { -1, 0, 1 };
             var ys = new[] { -1, 0, 1 };
             var shifts =
@@ -304,30 +296,29 @@ namespace GameLogic
                              shifts,
                              pieceCells,
                              enemyPieceCells,
-                             OnBoard,
+                             IsOnBoard,
                              oneShift: true);
         }
 
+        // Finds cells under threat produced by pawn at cell |cell|.
+        //
+        // Parameters
+        // ----------
+        // cell: The cell.
+        // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
+        // enemyPieceCells: A list containing pieces that belong to the enemy color.
+        // IsOnBoard: A function to decide on where the cell is on board.
+        // activeColor: The pawn color.
+        //
+        // Returns
+        // -------
+        // A list containing cells under threat produced by pawn at cell |cell|.
         public static List<Cell> GetCellsUnderThreatPawn(Cell cell,
                                                          List<Cell> pieceCells,
                                                          List<Cell> enemyPieceCells,
-                                                         Func<Cell, bool> OnBoard,
+                                                         Func<Cell, bool> IsOnBoard,
                                                          Color activeColor)
         {
-            // Finds cells under threat produced by pawn at cell |cell|.
-            //
-            // Parameters
-            // ----------
-            // cell: The cell.
-            // pieceCells: A list containing pieces that belong to the same color as the piece at cell |cell|.
-            // enemyPieceCells: A list containing pieces that belong to the enemy color.
-            // OnBoard: A function to decide on where the cell is on board.
-            // activeColor: The pawn color.
-            //
-            // Returns
-            // -------
-            // A list containing cells under threat produced by pawn at cell |cell|.
-
             var leftShift = activeColor == Color.White ? new Cell(-1, 1) : new Cell(-1, -1);
             var rightShift = activeColor == Color.White ? new Cell(1, 1) : new Cell(1, -1);
 
