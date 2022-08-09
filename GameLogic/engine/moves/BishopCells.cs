692dcf7f8a97aa4fff0d71b205d2395ed433fddd
SherlockKA <kurzyukovandrew@mail.ru>
2022-08-09 23:56:59 +0200

    Rename a file

--- GameLogic/engine/moves/BishopCells.cs
+++ GameLogic/engine/moves/BishopCells.cs
@@ -1,7 +1,5 @@
 using GameLogic.Entities;
 
-// TODO: Dear @SherlockKA, please rename this file and all others that are related to the "cells".
-
 namespace GameLogic.Engine.Moves
 {
     public class BishopCells : IPieceCells
